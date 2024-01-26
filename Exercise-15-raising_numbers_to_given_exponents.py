d664a8c00266cfeeeaa9eea7c3b674077bde8524
unknown <miggy.payumo26@gmail.com>
2024-01-27 00:13:14 +0800

    Code added

--- Exercise-15-raising_numbers_to_given_exponents.py
+++ Exercise-15-raising_numbers_to_given_exponents.py
@@ -2,3 +2,13 @@
 # 1. Using def functions create a function called exponent(base,exp)
 # 2. Return the int vaulue of base raises to the power of exp
 
+def exponent(base, exp):
+    exponent_number = exp
+    result = 1
+    while exponent_number > 0:
+        result = result * base
+        exponent_number = exponent_number - 1 
+    
+    print(base, "raised to", exp, "is: ", result)
+
+exponent(5, 4)
